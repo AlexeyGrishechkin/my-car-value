bf3df2849bdef5c5181538b67a37aef9dce1f329
agrishechkin <a.grishechkin@itransition.com>
2024-05-12 14:47:56 +0300

    add update-user.dto file, add CRUD methods to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() { email, password }: CreateUserDto) {
     this.userService.create(email, password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findById(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
